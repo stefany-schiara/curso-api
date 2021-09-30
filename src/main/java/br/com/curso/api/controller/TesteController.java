b2657bb7acd40bd3c0875723a1d87423cfa8e817
stefany-schiara <esquiara@gmail.com>
2021-09-30 17:25:38 -0300

    criando um @RestController

---
+++ src/main/java/br/com/curso/api/controller/TesteController.java
@@ -0,0 +1,21 @@
+package br.com.curso.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/teste")
+public class TesteController {
+
+	@GetMapping("/oi")
+	public String getOi() {
+		return "Oi Pessoal!";
+	}
+	
+	@GetMapping("/ola")
+	public String getOla() {		
+		return "Olá Pessoal!";
+	}
+	
+}
